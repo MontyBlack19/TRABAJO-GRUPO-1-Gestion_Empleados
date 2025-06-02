534271afd07728a47a66fc1f9445f8170d19ca28
victor <elliottsmith_steven@hotmail.com>
2025-06-01 22:42:41 -0500

    Se agregó turnodao

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Gestion_Empleados.Models;
+using Gestion_Empleados.Operaciones;
 using Gestion_Empleados.Operations;
 
 TipoEmpleadoDAO opTipoEmpleado = new TipoEmpleadoDAO();
@@ -39,4 +40,20 @@ var empletipo = opTipoEmpleado.seleccionarEmpleadosTipo();
 foreach (EmpleadoTipo empleadoTipo in empletipo)
 {
     Console.WriteLine(empleadoTipo.NombreEmpleado + " => " + empleadoTipo.NombreTipo);
+}
+
+Console.WriteLine("********************************************");
+
+static void MostrarTurnos(TurnoDAO dao)
+{
+    List<Turno> turnos = dao.seleccionarTodos();
+
+    Console.WriteLine("=== Lista de Turnos ===");
+    foreach (var turno in turnos)
+    {
+        Console.WriteLine($"Nombre: {turno.Nombre}, Entrada: {turno.HoraEntrada}, Salida: {turno.HoraSalida}");
+    }
+
+    Console.WriteLine("Presione una tecla para continuar...");
+    Console.ReadKey();
 }
---
+++ Gestion_Empleados/Operations/TurnoDAO.cs
@@ -0,0 +1,106 @@
+﻿using Gestion_Empleados.Models;
+using Gestion_Empleados.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Empleados.Operaciones
+{
+    public class TurnoDAO
+    {
+        public GestionEmpleados3Context contexto = new GestionEmpleados3Context();
+
+
+        public List<Turno> seleccionarTodos()
+        {
+            var turnos = contexto.Turnos.ToList<Turno>();
+            return turnos;
+        }
+
+        public Turno seleccionarTurno(int id)
+        {
+            var turno = contexto.Turnos.Where(a => a.IdTurno == id).FirstOrDefault();
+
+            return turno;
+        }
+
+        public bool insertar(string nombre, TimeOnly horaentrada, TimeOnly horasalida)
+        {
+            try
+            {
+                Turno turno = new Turno
+                {
+                    Nombre = nombre,
+                    HoraEntrada = horaentrada,
+                    HoraSalida = horasalida
+                };
+
+                contexto.Turnos.Add(turno);
+                contexto.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al insertar turno: " + ex.Message);
+                return false;
+            }
+        }
+
+
+        public bool actualizar(int id, string tipoturno, TimeOnly horaentrada, TimeOnly horasalida)
+        {
+            try
+            {
+                var turno = seleccionarTurno(id);
+
+                if (turno == null)
+                {
+                    return false;
+                }
+                else
+                {
+
+                    turno.Nombre = tipoturno;
+                    turno.HoraEntrada = horaentrada;
+                    turno.HoraSalida = horasalida;
+
+                    contexto.SaveChanges();
+
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool eliminar(int id)
+        {
+            try
+            {
+                var turno = seleccionarTurno(id);
+
+                if (turno == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    contexto.Turnos.Remove(turno);
+                    contexto.SaveChanges();
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
