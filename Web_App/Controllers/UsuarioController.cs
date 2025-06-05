03b04b84be3d0b8236a573ee70de2a1a8bf2a689
Mark <zuzanibar123@gmail.com>
2025-06-05 16:10:55 -0500

    Se agregi la Clase UsuarioDAO y el Controller Usuario con sus respectivos métodos y funcionales.

---
+++ Web_App/Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using Gestion_Empleados.Models;
+using Gestion_Empleados.Operations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_App.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private UsuarioDAO usuariodao = new UsuarioDAO();
+
+        [HttpGet("ListarUsuarios")]
+        public List<Usuario> mostrarUsuarios()
+        {
+            return usuariodao.listar();
+        }
+
+        [HttpDelete("EliminarUsuario")]
+        public bool eliminarUsuario(int id)
+        {
+            return usuariodao.eliminar(id);
+        }
+
+        [HttpPut("ActualizarUsuario")]
+        public bool actualizarUsuario(int id, bool activo)
+        {
+            return usuariodao.actualizar(id, activo);
+        }
+
+        [HttpPost("InsertarUsuario")]
+        public bool insertarUsuario(string user)
+        {
+            return usuariodao.insertar(user);
+        }
+    }
+}
