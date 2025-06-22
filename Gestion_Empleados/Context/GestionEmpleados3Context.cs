bc89d75e00ac6e083bfe391ebe17a039098e2f6e
Victor Evangelista <elliottsmith_steven@hotmail.com>
2025-06-21 20:30:34 -0500

    Se agregó key  de appsettings

--- Gestion_Empleados/Context/GestionEmpleados3Context.cs
+++ Gestion_Empleados/Context/GestionEmpleados3Context.cs
@@ -40,7 +40,7 @@ public partial class GestionEmpleados3Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-KARMA\\SQLEXPRESS;Database=Gestion_Empleados;Trust Server Certificate=true;User Id=Marco;Password=12345;MultipleActiveResultSets=true");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-NULVUEN\\SQLEXPRESS;Database=Gestion_Empleados3;Trust Server Certificate=true;User Id=Victor;Password=sqladmin;MultipleActiveResultSets=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
